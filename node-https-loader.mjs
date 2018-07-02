dadd404a5f406fe779fcfc739e17b81a552089e5
Viktor Kuroljov <viktor.konsta@gmail.com>
2018-07-03 00:41:36 +0300

    Basic experimental version

---
+++ node-https-loader.mjs
@@ -0,0 +1,43 @@
+import fs from 'fs';
+import path from 'path';
+import process from 'process';
+
+import norm from './lib/norm.mjs';
+import hash from './lib/hash.mjs';
+import fetchModule from './lib/fetchModule.mjs';
+
+const JS_EXTENSIONS = new Set(['.js', '.mjs']);
+
+export async function resolve(specifier, parentModuleURL, defaultResolver) {
+  if (!specifier.startsWith('https:')) {
+    return defaultResolver(specifier, parentModuleURL);
+  }
+
+  const destDir = `${norm(process.env.NODE_HTTP_MODULES || '/tmp')}/node_http_modules`;
+
+  if (!fs.existsSync(destDir)) {
+    fs.mkdirSync(destDir);
+  }
+
+  const scriptPath = new URL(`file://${destDir}/${hash(specifier)}.mjs`);
+
+  let source;
+
+  if (fs.existsSync(scriptPath)) {
+    source = fs.readFileSync(scriptPath, 'utf8');
+  } else {
+    source = await fetchModule(specifier);
+    fs.writeFileSync(scriptPath, source);
+  }
+
+  const ext = path.extname(scriptPath.pathname);
+
+  if (!JS_EXTENSIONS.has(ext)) {
+    throw new Error(`Cannot load file with non-JavaScript file extension ${ext}.`);
+  }
+
+  return {
+    url: scriptPath.href,
+    format: 'esm',
+  };
+}
---
+++ test.mjs
@@ -0,0 +1,13 @@
+/*
+  eslint-disable
+    import/extensions,
+    import/no-unresolved
+  */
+
+import assert from 'assert';
+
+import { a } from 'https://raw.githubusercontent.com/kuroljov/esm-test/master/a.js';
+import { b } from 'https://raw.githubusercontent.com/kuroljov/esm-test/master/b.js';
+
+assert.strictEqual(a, 'a');
+assert.strictEqual(b(), 'b');
